0f27528f63699fda7b932e18d2735bbd88e8ea13
linvi <linvi@users.noreply.github.com>
2016-12-13 23:36:21 +0000

    [1.2] Simple console application

---
+++ Console .NET 4.6.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tweetinvi;
+
+namespace Console.NET_4._6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+
+            var user = User.GetAuthenticatedUser();
+
+            System.Console.WriteLine(user);
+            System.Console.ReadKey();
+        }
+    }
+}
