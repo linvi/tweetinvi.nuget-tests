c6eb8878a0b4675b61015cefdf7da2c63e4c2828
linvi <linvi@users.noreply.github.com>
2016-12-14 00:22:16 +0000

    [1.2] Android App

---
+++ Android 4.6.2/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Tweetinvi;
+
+namespace Android_4._6._2
+{
+    [Activity(Label = "Android_4._6._2", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+
+            var user = User.GetAuthenticatedUser();
+
+            button.Click += delegate { button.Text = string.Format("{0} clicked {1} times!", user, count++); };
+        }
+    }
+}
